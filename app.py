8072f4ac2bfd9245e168dc4f00f0e18a0dc18e24
TomoyaFujita2016 <fujita.t.2016c@gmail.com>
2022-01-26 01:10:00 +0900

    second commit

--- app.py
+++ app.py
@@ -8,19 +8,21 @@ from logger import log
 def get_subtitle(video_id, sentence_num: int = 10):
     vtt_path = download_subtitle(video_id)
     text = vtt2text(vtt_path)
-    text = divide_sentence(text)
-    summary = make_summary(text, sentence_num=10)
+    origin = divide_sentence(text)
+    summary = make_summary(origin, sentence_num=10)
     log.debug(summary)
-    return summary
+    return summary, origin
 
 
 st.title("Video Summary")
-sentence_num = st.slider("要約後の文の数", min_value=0, max_value=100, step=1, value=10)
-video_id = st.text_input(label="video_id(https://www.youtube.com/watch?v={video_id})", value="xlDGAr5FAvA")
-run_subtitle = st.button("実行")
+sentence_num = st.slider("・要約後の文の数", min_value=0, max_value=100, step=1, value=10)
+video_id = st.text_input(label="・video_id (ヒント: https://www.youtube.com/watch?v={video_id})", value="xlDGAr5FAvA")
+run_summary = st.button("実行")
 
 
-if run_subtitle:
-    subtitle = get_subtitle(video_id, sentence_num=sentence_num)
+if run_summary:
+    summary, origin = get_subtitle(video_id, sentence_num=sentence_num)
     st.subheader("実行結果")
-    st.text(subtitle)
+    st.text(summary)
+    with st.expander("原文"):
+        st.write(origin)
